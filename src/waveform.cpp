e76973c1c752cf3c8ce4b07d58de9250e95d6f46
Roman Serov <roman@serov.co>
2019-03-21 22:52:02 +0300

    Лицензия и readme

--- src/waveform.cpp
+++ src/waveform.cpp
@@ -1,3 +1,26 @@
+/*
+ * Generator control tool
+ *
+ * Copyright 2019 by Roman Serov <roman@serov.co>
+ *
+ * This file is part of Generator control tool.
+ *
+ * Generator control tool is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Generator control tool is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Generator control tool. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+*/
+
 #include "waveform.h"
 
 Waveform::Waveform::Waveform(string val) {
